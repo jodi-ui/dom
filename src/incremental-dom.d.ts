7af539002615e770e288c252df6b6ab11ecc24a1
Aleksander Ciesiołkiewicz <a.ciesiolkiewicz@gmail.com>
2016-08-28 18:58:52 +0200

    typescript module resolution settings

--- src/incremental-dom.d.ts
+++ src/incremental-dom.d.ts
@@ -1,5 +1,7 @@
-export function elementOpen(tag:string, key?:string, statics?:Array<any>, ...const_args):Element;
-export function elementClose(tag:string):Element;
-export function elementVoid(tag:string, key?:string, statics?:Array<any>, ...const_args):Element;
-export function text(value, ...const_args);
-export function patch(node:Element, fn:Function, data?:any);
+declare module 'incremental-dom' {
+    export function elementOpen(tag:string, key?:string, statics?:Array<any>, ...const_args):Element;
+    export function elementClose(tag:string):Element;
+    export function elementVoid(tag:string, key?:string, statics?:Array<any>, ...const_args):Element;
+    export function text(value, ...const_args);
+    export function patch(node:Element, fn:Function, data?:any);
+}
