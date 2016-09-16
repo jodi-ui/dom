38042dc3eda9dd1413308d025ba92c3ab54c7ac8
Aleksander Ciesiołkiewicz <a.ciesiolkiewicz@gmail.com>
2016-09-16 07:34:34 +0200

    export few more functions and classes

--- index.ts
+++ index.ts
@@ -1,4 +1,7 @@
 import {dynamicProps, staticProps, key} from './src/functions';
+
+export {dynamicProps, staticProps, key} from './src/functions';
+export {DynamicProperties, StaticProperties, Key} from './src/classes';
 export * from './src/dom';
 
 // shortcuts
